cd38d1db1ffe58d4329911aa70a45fe3cf8604b2
RafaelSoftDev <rafael.software.ing@gmail.com>
2026-06-04 13:05:58 -0500

    Nombre en el README y tablas en el sql

--- scripts_sql/01_ddl_estructura.sql
+++ scripts_sql/01_ddl_estructura.sql
@@ -6,26 +6,65 @@ CREATE TABLE cargo (
 
 --Segunda entidad: Cliente DL
 CREATE TABLE cliente (
-    id_cli SERIAL PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
-    dni VARCHAR(15) NOT NULL UNIQUE,
-    telefono VARCHAR(20),
-    email VARCHAR(100) UNIQUE,
-    direccion VARCHAR(200)
+    id_cli SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    dni VARCHAR(15) NOT NULL UNIQUE,
+    telefono VARCHAR(20),
+    email VARCHAR(100) UNIQUE,
+    direccion VARCHAR(200)
 );
 
 --Tercera entidad: Proveedor DL
 CREATE TABLE proveedor (
-    id_prov SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    ruc VARCHAR(11) NOT NULL UNIQUE,
-    telefono VARCHAR(20),
-    email VARCHAR(100),
-    direccion VARCHAR(200)
+    id_prov SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    ruc VARCHAR(11) NOT NULL UNIQUE,
+    telefono VARCHAR(20),
+    email VARCHAR(100),
+    direccion VARCHAR(200)
 );
 
+-- Tercera entidad: Categoría
+CREATE TABLE categoria{
+    id_cat INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL UNIQUE
+};
 
+-- Cuarta entidad: Marca
+CREATE TABLE marca{
+    id_marca INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL UNIQUE
+};
+
+-- Quinta entidad: Smartphone
+CREATE TABLE smartphone{
+    id_smart SERIAL PRIMARY KEY,
+    modelo VARCHAR(100) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    stock INT,
+    especificaciones TEXT,
+    -- Utilización de claves foráneas
+    -- Claves de la tabla marca y categoria
+    -- Formas corta
+    -- id_marca INT REFERENCES marca(id_marca),
+    -- id_cat INT REFERENCES categoria(id_cat)
+    -- Forma correcta
+    id_marca INT,
+    id_cat INT,
+    CONSTRAINT fk_smartphone_marca 
+        FOREIGN KEY (id_marca) REFERENCES marca(id_marca),
+    CONSTRAINT fk_smartphone_categoria 
+        FOREIGN KEY (id_cat) REFERENCES categoria(id_cat)
+};
+
+-- Sexta entidad: Sucursal
+CREATE TABLE Sucursal{
+    id_suc SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL UNIQUE,
+    direccion VARCHAR(100) NOT NULL UNIQUE,
+    telefono VARCHAR(20) NOT NULL UNIQUE
+};
 
 --Novena entidad: Employees
 CREATE TABLE employees(
@@ -65,7 +104,7 @@ CREATE TABLE detalle_venta(
 );
 
 --Duodécima entidad: Metodo_pago
-CREATE TABLE metodo_pago(
+-- CREATE TABLE metodo_pago(
     id_mpago SERIAL PRIMARY KEY,
     descripcion VARCHAR(50) NOT NULL
 );
