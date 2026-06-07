d4d6e745db832b2c15789eb8c81c8cc9d12f6a4f
Derek Loayza <derek.loayza@gmail.com>
2026-06-07 13:03:17 -0500

    fear: Creacion de archivo Llenar datos

---
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -0,0 +1,3 @@
+-- Inserts básicos de prueba 
+INSERT INTO SMARTPHONE (id_smart, modelo, precio, stock, especificaciones, id_marca, id_cat) 
+VALUES (1, 'Galaxy S24', 3500.00, 20, '256GB, 8GB RAM', 1, 1);
