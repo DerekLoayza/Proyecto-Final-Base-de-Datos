a7f58efa07b86ac94f36db3652d8bda3ac5ded0f
RafaelSoftDev <rafael.software.ing@gmail.com>
2026-06-07 17:17:13 -0500

    Reporte y Analisis

--- scripts_sql/04_reportes_y_analisis.sql
+++ scripts_sql/04_reportes_y_analisis.sql
@@ -1,1 +1,10 @@
 --Reporte
+SELECT 
+    m.nombre AS marca, 
+    SUM(d.cantidad) AS total_equipos_vendidos, 
+    SUM(d.cantidad * d.precio_unitario) AS ingresos_totales
+FROM MARCA m
+JOIN SMARTPHONE s ON m.id_marca = s.id_marca
+JOIN DETALLE_VENTA d ON s.id_smart = d.id_smart
+GROUP BY m.nombre
+HAVING SUM(d.cantidad * d.precio_unitario) > 1000;
